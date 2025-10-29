56044983517e038f139e598832d201af7de5df9c
Viktória Latičová <xlaticova@stuba.sk>
2025-10-29 21:04:39 +0100

    o nás - tím

--- Votum_Viki/web/resources/views/pages/aboutus.blade.php
+++ Votum_Viki/web/resources/views/pages/aboutus.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto mb-12 pt-10 px-6">
+    <div class="container mx-auto pb-6 pt-10 px-6">
 
         <div class="flex flex-col md:flex-row justify-between items-center text-center md:text-left gap-6 mb-10">
             <h1 class="text-5xl md:text-6xl font-extrabold text-votum-blue">
