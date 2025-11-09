a6860d6567269cb972f5c8e1ce04377057fb6ec7
Viktória Latičová <xlaticova@stuba.sk>
2025-11-09 16:14:35 +0100

    podpora, uprava generovaneho kodu

--- Votum_Viki/web/resources/views/components/contacts/input.blade.php
+++ Votum_Viki/web/resources/views/components/contacts/input.blade.php
@@ -1,15 +1,15 @@
 @props(['name', 'value'])
 
 <div>
-    <p class="text-sm text-gray-600 mb-1">{{$name}}:</p>
+    <p class="text-lg text-bold text-gray-900 mb-1">{{$name}}:</p>
     <div class="flex items-center justify-between bg-votum-cream p-3 rounded">
-        <span class="font-semibold">{{$value}}</span>
+        <span class="font-semibold text-xl">{{$value}}</span>
 
         <button
             onclick="copyToClipboard('{{$value}}')"
                 class="copy-btn text-gray-600 active:scale-90 transition transform duration-150 ease-in-out"
                 title="{{$name}}">
-            <i class="fas fa-copy text-lg"></i>
+            <i class="fas fa-copy text-3xl"></i>
         </button>
 
     </div>
--- Votum_Viki/web/resources/views/components/home.blade.php
+++ Votum_Viki/web/resources/views/components/home.blade.php
@@ -1,6 +1,6 @@
 <!-- Home Button -->
 <div class="text-center mt-16 mb-8">
-    <a href="{{route('main')}}" class="inline-flex items-center gap-3 bg-votum-blue text-white px-8 py-4 rounded-lg hover-scale font-semibold text-lg shadow-lg">
+    <a href="{{route('main')}}" class="inline-flex items-center gap-3 bg-votum-blue text-white px-10 py-8 rounded-lg hover-scale font-semibold text-xl shadow-lg">
         <i class="fas fa-home text-2xl"></i>
         <span>Domov</span>
     </a>
