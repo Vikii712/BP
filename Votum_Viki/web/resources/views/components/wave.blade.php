a0cb198f52db603901eece0ef7a7e1002d684f8e
ChatGPT <chatgpt@openai.com>
2025-11-11 14:27:56 +0100

    vlnite oddelenie sekcii
    oddelenie dvoch farieb pomocou svg vlnky

---
+++ Votum_Viki/web/resources/views/components/wave.blade.php
@@ -0,0 +1,22 @@
+<!-- VLNITÝ ODDIEL medzi sekciami -->
+<div class="relative -mt-[2px]">
+    <svg viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg" class="block w-full">
+        <!-- Horná krémová časť -->
+        <path fill="#fff" d="
+            M0,60
+            C180,30 360,30 540,60
+            C720,90 900,90 1080,60
+            C1260,30 1440,30 1620,60
+            L1620,0 L0,0 Z
+        "></path>
+
+        <!-- Spodná modrá časť -->
+        <path fill="#dbeafe" d="
+            M0,60
+            C180,30 360,30 540,60
+            C720,90 900,90 1080,60
+            C1260,30 1440,30 1620,60
+            L1620,100 L0,100 Z
+        "></path>
+    </svg>
+</div>
